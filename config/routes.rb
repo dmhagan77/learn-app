4b0f1f82eff942d7b3d080dade2ccc6cb9457534
David M. Hagan <dahagan77@gmail.com>
2014-02-13 11:45:58 -0800

    attempting to get heroku working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 HagaLearningApp::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   resources :words
   root 'words#index'
-  post '/', to: "words#index"
+
+  match '/' , to: "words#index" , via: "post"
 end
